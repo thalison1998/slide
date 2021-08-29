6007438564079b510372d9923a984a00d0c90974
Thalison Campos <thalison1998@gmail.com>
2021-08-28 22:40:45 -0300

    eventos de touch adicionados

--- js/slide.js
+++ js/slide.js
@@ -3,12 +3,11 @@
     const slide = document.querySelector(slideSelect)
     const wrapper = document.querySelector(wrapperSelect)
     const dist = { finalPosition:0, startX:0, movement:0,}
-   
+    
     function moveSlide(distX){
         dist.movePosition = distX
         slide.style.transform = `translate3d(${distX}px, 0, 0)`
     }
-
 
     function updatePosition(clientX){
         dist.movement = (dist.startX - clientX) * 2
@@ -16,24 +15,39 @@
     }
 
     function onMove(e){
-        const finalPosition = updatePosition(e.clientX)
+        const pointerPosition = (e.type === 'mousemove')
+        ?e.clientX:e.changedTouches[0].clientX
+        const finalPosition = updatePosition(pointerPosition)
         moveSlide(finalPosition);
     }
     function onStart(e){
-        e.preventDefault();
-        dist.startX = e.clientX
-        wrapper.addEventListener('mousemove',onMove)
+        let movetype;
+        if(e.type === 'mousedown'){
+             e.preventDefault();
+             movetype = 'mousemove'
+             dist.startX = e.clientX
+        }
+        else{
+            dist.startX = e.changedTouches[0].clientX
+            movetype = 'touchmove'
+        }
+        
+        wrapper.addEventListener(movetype,onMove)
     }
 
     function onEnd (e){
-        wrapper.removeEventListener('mousemove',onMove)
+        const movetype = (e.type === 'mouseup')?'mousemove':'touchmove'
+        wrapper.removeEventListener(movetype,onMove)
         dist.finalPosition = dist.movePosition
 
     }
 
     function addSlideEvents(){
         wrapper.addEventListener('mousedown',onStart)
+        wrapper.addEventListener('touchstart',onStart)
         wrapper.addEventListener('mouseup',onEnd)
+        wrapper.addEventListener('touchend',onEnd)
+        
     }
     function init(){
         addSlideEvents();
